9d09df5c8ce5479304ac4cb53664824c24adff99
sufyanjais <sufyanjais1@gmail.com>
2025-05-29 16:20:30 +0800

    push whole codebase

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MovieCard = ({movie: {title, vote_average, poster_path, release_date,
+     original_language}}) => {
+  return (
+    <div className='movie-card'>
+        <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` :
+        `/no-movie.png`} />
+    </div>
+  )
+}
+
+export default MovieCard
